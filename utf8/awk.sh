65322fa9cb38e0034ca9eb35bf72cf2b52eb14cc
Eric Fischer <enf@pobox.com>
2017-12-03 18:24:46 -0800

    Use consistent formatting

--- utf8/awk.sh
+++ utf8/awk.sh
@@ -3,16 +3,16 @@
 LC_ALL=en_US.UTF-8
 export LC_ALL
 
-awk -F'💩'  '{print $2}' fixtures/awk1 > out/awk1.check
+awk -F'💩'  '{ print $2 }' fixtures/awk1 > out/awk1.check
 ./compare awk1
 
-awk '{print length}' fixtures/awk2 > out/awk2.check
+awk '{ print length }' fixtures/awk2 > out/awk2.check
 ./compare awk2
 
-awk 'BEGIN {print index("𝐇𝐞𝐥𝐥𝐨", "𝐨"); exit 0}' > out/awk3.check
+awk 'BEGIN { print index("𝐇𝐞𝐥𝐥𝐨", "𝐨"); exit 0 }' > out/awk3.check
 ./compare awk3
 
-awk 'BEGIN {print substr("𝐇𝐞𝐥𝐥𝐨 𝐰𝐨𝐫𝐥𝐝", 7); exit 0}' > out/awk4.check
+awk 'BEGIN { print substr("𝐇𝐞𝐥𝐥𝐨 𝐰𝐨𝐫𝐥𝐝", 7); exit 0 }' > out/awk4.check
 ./compare awk4
 
 awk '{ print tolower($0) }' fixtures/awk5 > out/awk5.check
