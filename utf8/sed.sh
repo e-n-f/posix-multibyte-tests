c7c7a904e173dc9cdfed108f30ade07dd602bf16
Eric Fischer <enf@pobox.com>
2017-12-05 14:09:59 -0800

    Test sed regular expression star

--- utf8/sed.sh
+++ utf8/sed.sh
@@ -7,3 +7,8 @@ cat fixtures/dd1 |
 sed 'y/Çàç/𝐂𝐚𝐜/' > out/sed1.check
 
 ./compare sed1 "Character substitution"
+
+cat fixtures/sed2 |
+sed 's/𝐥*/𝑙/' > out/sed2.check
+
+./compare sed2 "Regular expression star"
