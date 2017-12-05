de8987d812a4bf2539f86b7e1b5d552925e772c0
Eric Fischer <enf@pobox.com>
2017-12-04 16:41:17 -0800

    Test tr

---
+++ utf8/tr.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+LC_ALL=en_US.UTF-8
+export LC_ALL
+
+cat fixtures/dd1 |
+tr Çàç 𝐂𝐚𝐜 > out/tr1.check
+
+./compare tr1 "Character substitution"
+
+cat fixtures/dd1 |
+tr '\303\207\303\240\303\247' '360\235\220\202\360\235\220\232\360\235\220\234' > out/tr2.check
+
+./compare tr2 "Multibyte backslash representation"
