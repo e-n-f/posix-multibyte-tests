ce6929784c449b1948a340acd431f3b45a004864
Eric Fischer <enf@pobox.com>
2017-12-26 13:55:12 -0800

    Test consistency between single-byte and multibyte

--- utf8/pr.sh
+++ utf8/pr.sh
@@ -15,5 +15,14 @@ pr -l10 -n💩5 fixtures/names > out/pr3.check
 cat fixtures/names | pr -l10 -10 > out/pr4.check
 ./compare pr4 "Multiple columns"
 
+cat fixtures/names | pr -l10 -10 -s: | sed 's/:/💩/g' > out/pr5
 cat fixtures/names | pr -l10 -10 -s💩 > out/pr5.check
 ./compare pr5 "Multiple columns with separator character"
+
+cat fixtures/names | iconv -f utf-8 -t iso_8859-1 | LC_ALL=en_US.ISO8859-1 pr -l10 -10 -s: | iconv -f iso_8859-1 -t utf-8 | sed 's/:/💩/g' > out/pr6
+cat fixtures/names | pr -l10 -10 -s💩 > out/pr6.check
+./compare pr6 "Multiple columns with separator character, via 8859-1"
+
+cat fixtures/names | iconv -f utf-8 -t iso_8859-1 | LC_ALL=en_US.ISO8859-1 pr -l10 -10 | iconv -f iso_8859-1 -t utf-8 > out/pr7
+cat fixtures/names | pr -l10 -10 > out/pr7.check
+./compare pr7 "Multiple columns via 8859-1"
