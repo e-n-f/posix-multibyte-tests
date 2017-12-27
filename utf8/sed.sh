1e2df55f3f35ce49d608937da70177fd3a5b9462
Eric Fischer <enf@pobox.com>
2017-12-27 08:50:13 -0800

    More sed variations

--- utf8/sed.sh
+++ utf8/sed.sh
@@ -12,3 +12,13 @@ cat fixtures/sed2 |
 sed 's/𝐥*/𝑙/' > out/sed2.check
 
 ./compare sed2 "Regular expression star"
+
+cat fixtures/sed2 |
+sed 's/[𝐥]*//g' > out/sed3.check
+
+./compare sed3 "Regular expression star with brackets"
+
+cat fixtures/sed2 |
+sed 's/𝐥.//g' > out/sed4.check
+
+./compare sed4 "Regular expression dot"
